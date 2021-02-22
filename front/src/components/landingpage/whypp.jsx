97f084fa122f7aeb8e9b7cf794639085a9115b30
jerosantamariai <jerosantamariai@gmail.com>
2021-02-22 18:52:58 -0300

    wpp component ready and some fixes

--- front/src/components/landingpage/whypp.jsx
+++ front/src/components/landingpage/whypp.jsx
@@ -4,17 +4,60 @@ const WhyPP = props => {
     return (
         <section className="container3" name="whypp" id="whypp">
             <div className="d-flex mx-auto">
-                <h1>¿Por qué elegir ProtegePyme?</h1>
-                <ul>
-                    <li>Partner estratégico de CobranzaOnline, líder en la cobranza extrajudicial de facturas.</li>
-                    <li>Trabajamos 100% online, facilitando la comunicación con nuestros clientes</li>
-                    <li>Nos enfocamos 100% en las MiPymes</li>
-                    <li>Somos un equipo legal experto en cobranzas.</li>
-                    <li>Reportamos periódicamente del estado de los juicios para que puedas supervisarlos.</li>
-                    <li>Trabajamos omnicanal, para responder cualquier consulta en menos de 24 hrs.</li>
-                    <li>Te incorporamos a nuestro software. Cada vez que exista un movimiento, se te notificará a tu correo.</li>
-                    <li>Transparentamos nuestros precios, para que no te lleves ninguna sorpresa.</li>
-                </ul>
+                <div className="row d-flex justify-content-center">
+                    <div className="col-5">
+                        <div className="col-8 offset-2">
+                            <h1>¿Por qué elegir ProtegePyme?</h1>
+                            <p>Nuestro trabajo es garantía de compromiso, dedicación y experiencia</p>
+                        </div>
+                    </div>
+                    <div className="col-7 d-flex">
+                        <div className="col-6 mr-5">
+                            <div className="wppcontent mb-4">
+                                <i className="fas fa-handshake fa-2x"></i>
+                                <h4 className="py-3">Partner estratégico de CobranzaOnline</h4>
+                                <p>Líder en la cobranza extrajudicial de facturas.</p>
+                            </div>
+                            <div className="wppcontent mb-4">
+                                <i class="fas fa-wifi fa-2x"></i>
+                                <h4 className="py-3">Trabajamos 100% online</h4>
+                                <p>Facilitamos la comunicación con nuestros clientes</p>
+                            </div>
+                            <div className="wppcontent mb-4">
+                                <i class="fas fa-briefcase fa-2x"></i>
+                                <h4 className="py-3">Nos enfocamos 100% en Pymes</h4>
+                                <p>Por que son lo más importante para nosotros!</p>
+                            </div>
+                            <div className="wppcontent mb-4">
+                                <i class="fas fa-gavel fa-2x"></i>
+                                <h4 className="py-3">Expertos en cobranzas</h4>
+                                <p>Nuestro equipo es cuentan con años de experiencía</p>
+                            </div>
+                        </div>
+                        <div className="col-6 mt-5">
+                            <div className="wppcontent mb-4">
+                                <i class="far fa-newspaper fa-2x"></i>
+                                <h4 className="py-3">Reportes Periódicos</h4>
+                                <p>Nuestra forma de trabajo implica que puedas ser parte de nuestro equipo y puedas supervisar el trabajo</p>
+                            </div>
+                            <div className="wppcontent mb-4">
+                                <i class="fas fa-mobile-alt fa-2x"></i>
+                                <h4 className="py-3">Trabajo Omnicanal</h4>
+                                <p>Para responder cualquier consulta en menos de 24 hrs.</p>
+                            </div>
+                            <div className="wppcontent mb-4">
+                                <i class="fas fa-at fa-2x"></i>
+                                <h4 className="py-3">Notificaciones via eMail</h4>
+                                <p>Te incorporamos a nuestro software. Cada vez que exista un movimiento.</p>
+                            </div>
+                            <div className="wppcontent mb-4">
+                                <i class="fas fa-dollar-sign fa-2x"></i>
+                                <h4 className="py-3">Jamás te llevarás una sorpresa</h4>
+                                <p>Trasparentamos nuestros precios para tu tranquilidad y seguridad</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="break py-5">
                 <div className="aviso mx-auto py-5 px-3">
