5f33bf8296669b62b1650864990a547480f8eaea
jerosantamariai <jerosantamariai@gmail.com>
2021-03-02 20:51:40 -0300

    fixing the form

--- front/src/components/landingpage/form.jsx
+++ front/src/components/landingpage/form.jsx
@@ -11,25 +11,30 @@ const Form = props => {
                     </div>
                 </div>
                 <div>
-                    <form className="col-md-8 offset-md-2 position-inherit">
+                    <form className="col-md-8 offset-md-2 position-inherit" method="post" action="enviar.php">
                         <div className="form-row" >
                             <div className="col position-inherit" >
-                                <input type="text" className="form-control my-2" placeholder="Nombre y Apellido" />
+                                <input type="text" className="form-control my-2" name="nombre" required="required" placeholder="Nombre y Apellido" />
                             </div>
                         </div>
                         <div className="form-row" >
                             <div className="col position-inherit">
-                                <input type="text" className="form-control my-2" placeholder="Teléfono - +569XXXXXXXX" />
+                                <input type="text" className="form-control my-2" name="tel" required="required" placeholder="Teléfono - +569XXXXXXXX" />
                             </div>
                         </div>
                         <div className="form-row" >
                             <div className="col position-inherit">
-                                <input type="email" className="form-control my-2" placeholder="Correo Electrónico" />
+                                <input type="email" className="form-control my-2" name="email" required="required" placeholder="Correo Electrónico" />
                             </div>
                         </div>
                         <div className="form-row">
                             <div className="col position-inherit">
-                                <textarea class="form-control my-2" id="textArea" rows="8" placeholder="Cuentame aquí como te puedo ayudar..."></textarea>
+                                <textarea class="form-control my-2" id="textArea" rows="8" name="info" required="required" placeholder="Cuentame aquí como te puedo ayudar..."></textarea>
+                            </div>
+                        </div>
+                        <div className="form-row" >
+                            <div className="col position-inherit">
+                                <button className="btn btn-primary" type="submit">Enviar</button>
                             </div>
                         </div>
                         <div>
